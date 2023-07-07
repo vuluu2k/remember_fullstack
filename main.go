9468ed94c326e2e88d46aac75513e7a6afd32ba1
vuluu <vuluu040320@gmail.com>
2023-07-07 15:26:53 +0700

    ok

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	log.Println("Starting server...")
+	router := gin.Default()
+
+	router.GET("api/auth", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World!",
+		})
+	})
+	svr := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		if err := svr.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Fail to initialize server: %v\n", err)
+		}
+	}()
+
+	log.Printf("Listening in http://localhost%v", svr.Addr)
+
+	quit := make(chan os.Signal)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	log.Println("Shutting down server...")
+
+	if err := svr.Shutdown(ctx); err != nil {
+		log.Fatalf("Fail to shutdown server: %v\n", err)
+	}
+}
